cb431da2e3dc90349bb0369f3435815c74a2a7c8
Osnildo <juniorosnildo777@gmail.com>
2025-04-09 20:24:01 -0300

    Adionada a dunção de validar numero impares

---
+++ DiamondGenerator/Diamante.cs
@@ -0,0 +1,12 @@
+﻿namespace DiamondGenerator;
+
+public class Diamante
+{
+    
+    //VALIDA O VALOR DE ENTRADA
+    public static bool ValidarNumero(int numero)
+    {
+        return numero % 2 != 0;
+    }
+    
+}
---
+++ DiamondGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using DiamondGenerator;
+
+bool run = true;
+
+while (run)
+{
+    bool validar = true;
+
+    while (validar)
+    {
+        Console.Clear();
+        Console.WriteLine("Digite um valor ímpar");
+        var num = int.Parse(Console.ReadLine() ?? "3");
+
+        if (Diamante.ValidarNumero(num)) validar = false;
+
+    }
+
+}
